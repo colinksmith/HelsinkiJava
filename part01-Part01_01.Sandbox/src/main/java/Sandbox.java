919e8a97880a8eb401489355110d6e7c5b41904b
York MacBook 079 <yorkmacbook079@Yorks-MacBook-Pro.local>
2024-02-13 11:49:17 -0500

    initial commit completed assignments 1-9

---
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -0,0 +1,9 @@
+
+public class Sandbox {
+
+    public static void main(String[] args) {
+        // Nothing
+        System.out.println("ada lovelace");
+        System.out.println("This is a second line");
+    }
+}
